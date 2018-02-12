644aa94a5b13ea460519eb81c260ddbe993b4d7a
Dennys José Márquez Reyes <dennysjmarquez@gmail.com>
2018-02-12 17:27:01 -0400

    Add files via upload

--- js/AnimateFrameObject.js
+++ js/AnimateFrameObject.js
@@ -1,67 +1,53 @@
-var AnimateFrameObject = function() {
+	function AnimateFrameObject() {
+
+		this.inifps = 0, this.elapsed, this.now;
+		this.then, this.callback, this.aniTimeID;
+
+	}
+
+	AnimateFrameObject.prototype.run = function(callback, seconds) {
+		
+		var instance = this;
+			
+		function loop() {
+		
+			instance.aniTimeID = instance.__proto__.requestAnimFrame.call(window, loop);
+			instance.now = Date.now();
+			instance.elapsed = instance.now - instance.then;
+			
+			if (instance.elapsed > instance.inifps) {
+				
+				instance.then = instance.now - (instance.elapsed % instance.inifps);
+				instance.callback();
+				
+			}
+		
+		}
+		
+		instance.inifps = (seconds) ? seconds : instance.inifps;
+		instance.callback = (callback) ? callback : instance.callback;
+		instance.then = Date.now();
+		loop();
+		
+	}
 	
-	var requestAnimFrame = (function(){
-  
+	AnimateFrameObject.prototype.destroy = function(){
+	
+		var instance = this;
+		clearTimeout(this.aniTimeID), cancelAnimationFrame(this.aniTimeID);
+		Object.keys(instance).forEach(function(k){ instance[k] = null});
+		instance = undefined;
+		
+	}
+	
+	AnimateFrameObject.prototype.requestAnimFrame = (function(){
+		
 		return  window.requestAnimationFrame       || 
 				window.webkitRequestAnimationFrame || 
 				window.mozRequestAnimationFrame    || 
 				window.oRequestAnimationFrame      || 
 				window.msRequestAnimationFrame     || 
-				function(/* function */ callback, /* DOMElement */ element){
-					window.setTimeout(callback, 1000/60)
+				function(callback){
+					return window.setTimeout(callback, 1000/60)
 				};
 	})();
-
-	function run(callback, seconds) {
-		
-		new Interval().run(callback, seconds)
-		
-	}
-	
-	var Interval = function() {
-		
-		var inifps = 0;
-		var elapsed, now, then;
-		var call;
-		
-		function loop() {
-		
-			requestAnimFrame(loop);
-			now = Date.now();
-			elapsed = now - then;
-		
-			if (elapsed > inifps) {
-
-				then = now - (elapsed % inifps);
-				call();
-			
-			}
-	
-		}		
-		
-		function run(callback, fps){
-			
-			inifps = (fps) ? fps : inifps;
-			call = (callback) ? callback : call;
-			then = Date.now();
-			
-			loop();
-			
-		}
-		
-		return {
-		
-			"run": run
-			
-		};
-		
-	
-	}
-	
-	return {
-		
-		"run": run
-			
-	};
-
-}
